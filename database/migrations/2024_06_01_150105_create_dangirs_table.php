87fe531efb16671a75cf62b788465f80f2ba8428
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-01 22:09:18 +0700

    test: remove test

--- database/migrations/2024_06_01_150105_create_dangirs_table.php
+++ database/migrations/2024_06_01_150105_create_dangirs_table.php
@@ -19,8 +19,6 @@ return new class extends Migration
             $table->string('persentase');
             $table->enum('status', ['pending', 'verified'])->default('pending');
             $table->timestamps();
-            //test saja
-            //cobas
         });
     }
 
