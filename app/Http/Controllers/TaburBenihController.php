2a2daccaa717d7cdfa10e7a9926d58460710941d
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-01 21:58:38 +0700

    feat: add model cms tabur benih

---
+++ app/Http/Controllers/TaburBenihController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaburBenih;
+use Illuminate\Http\Request;
+
+class TaburBenihController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TaburBenih $taburBenih)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TaburBenih $taburBenih)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TaburBenih $taburBenih)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TaburBenih $taburBenih)
+    {
+        //
+    }
+}
