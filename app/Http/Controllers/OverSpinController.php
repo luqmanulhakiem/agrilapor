484f4178d6a258be25bef73b68cbcd2ecec571a1
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-01 21:59:15 +0700

    feat: add model cms overspin

---
+++ app/Http/Controllers/OverSpinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OverSpin;
+use Illuminate\Http\Request;
+
+class OverSpinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OverSpin $overSpin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OverSpin $overSpin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OverSpin $overSpin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OverSpin $overSpin)
+    {
+        //
+    }
+}
