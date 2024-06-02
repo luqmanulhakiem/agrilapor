5ea7f236fd05df82856e6a13feb2461422f59e89
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-02 21:53:17 +0700

    style: remove profil nav

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,10 +30,8 @@
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
+                
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profil</a>
-                </div>
-                <div class="pull-right">
                   <a href="{{route('logout.request')}}" class="btn btn-default btn-flat">Keluar</a>
                 </div>
               </li>
