bccc6d1499fd5b0b3e02c88d6c60ef29307f549f
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-02 12:26:24 +0700

    style: login form style, and routing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,13 +34,14 @@
   <div class="login-box-body">
     <p class="login-box-msg">Login untuk masuk</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="{{route('login.request')}}" method="post">
+      @csrf
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Username">
+        <input type="text" class="form-control" name="username" placeholder="Username">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
 })->name('login');
-Route::get('/dashboard', function () {
-    return view('halaman.Dashboard.index');
+Route::post('/login-request', [AuthController::class, 'login'])->name('login.request');
+
+Route::group(['middleware' => ['auth']], function (){
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('halaman.Dashboard.index');
+    });
 });
