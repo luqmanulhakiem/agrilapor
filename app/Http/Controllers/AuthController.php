0ce7a200a8ffef9eaec2134feba656f91d65f9ae
Luqman Project <luqmanulhakiem7@gmail.com>
2024-06-02 12:22:13 +0700

    feat: add login function

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request){
+        $data = $request->validated();
+
+        $find = User::where('username', $data['username'])->first();
+        if (!empty($find)) {
+            if (Hash::check($data['password'], $find->password)) {
+                Auth::attempt(['username' => $data['username'], 'password' => $data['password']]);
+                return redirect()->route('dashboard');
+            } else {
+                return redirect()->back()->withInput()->withErrors(['password anda salah']);
+            }
+        } else {
+            return redirect()->back()->withInput()->withErrors(['message' => 'username anda salah']);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
-});
+})->name('login');
 Route::get('/dashboard', function () {
     return view('halaman.Dashboard.index');
 });
